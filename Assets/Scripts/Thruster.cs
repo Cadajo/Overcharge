b5071b0938a8efea88ac21b192e4096658f87c21
Joel Lopez <jowie.jowie@gmail.com>
2018-01-27 00:47:21 +0100

    Fix Thruster adding rigidbodies

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Thruster : MonoBehaviour
 {
     private static readonly float MaxEnergy = 2;
