c27350782c946d75f2c9878bd08bf584864a940d
Joel Lopez <jowie.jowie@gmail.com>
2018-01-27 00:25:39 +0100

    Add thruster script

---
+++ Assets/Scripts/Thruster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Thruster : MonoBehaviour
+{
+    private static readonly float MaxEnergy = 2;
+
+    private float _energy = 0.0f;
+    private float _damage = 0.0f;
+
+    public Rigidbody SpaceshipRigidbody = null;
+
+    void Start()
+    {
+        if (SpaceshipRigidbody == null)
+        {
+            Debug.LogError("Spaceship rigidbody not set! :(");
+            enabled = false;
+        }
+    }
+
+    public void AddEnergy(float delta)
+    {
+        _energy = Mathf.Min(MaxEnergy, _energy + delta);
+    }
+
+    public void SubtractEnergy(float delta)
+    {
+        _energy = Mathf.Max(0, _energy - delta);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _damage += damage;
+    }
+
+    public float GetDamage()
+    {
+        return _damage;
+    }
+    
+    void FixedUpdate ()
+    {
+        SpaceshipRigidbody.AddForceAtPosition(transform.forward * _energy, transform.position);
+    }
+}
