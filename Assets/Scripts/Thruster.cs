53fb59c27438c9b3fd933a6ec435bb27f9c924a3
David da Silva <dasilvacontin@gmail.com>
2018-01-27 11:41:02 +0100

    dumb fix

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -75,6 +75,7 @@ public class Thruster : MonoBehaviour
                 break;
             }
             case DamageType.Overcharge:
+            {
                 multiplier = OverchargeDamageFactor;
                 break;
             }
