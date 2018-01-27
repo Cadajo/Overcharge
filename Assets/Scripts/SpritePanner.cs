782ba11cf6cdf06456c95efc7b88301329a58c78
Joel Lopez <jowie.jowie@gmail.com>
2018-01-27 20:00:20 +0100

    Add end line textures

---
+++ Assets/Scripts/SpritePanner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpritePanner : MonoBehaviour
+{
+    public float Offset = 0.1f;
+
+    private MeshRenderer _renderer;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _renderer = GetComponent<MeshRenderer>();
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        Vector2 offset = _renderer.material.mainTextureOffset;
+        offset.x = (offset.x + (Offset * Time.deltaTime)) % 1;
+        _renderer.material.mainTextureOffset = offset;
+    }
+}
