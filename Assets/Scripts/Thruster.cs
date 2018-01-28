1921e8bafd00d56f10b3a2726148b2ec3321f698
David da Silva <dasilvacontin@gmail.com>
2018-01-28 03:11:02 +0100

    add better ui

--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -38,8 +38,8 @@ public class Thruster : MonoBehaviour
         _trail.endWidth = _trail.startWidth * 1.2f;
         _trail.time = 1f;
         _trail.startColor = _trail.endColor = (_energy > 1.1f
-            ? Color.cyan
-            : Color.yellow);
+            ? Color.red
+            : Color.cyan);
     }
 
     public float AddEnergy(float delta)
---
+++ Assets/ThrusterBottomHUD.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterBottomHUD : MonoBehaviour {
+
+	public new MeshRenderer renderer;
+	public Thruster thruster;
+	// Use this for initialization
+	private static readonly int textureCount = 23;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int n = 0;
+		float energy = thruster.GetEnergy();
+		if (energy <= 1.1f) {
+			n = (int)Mathf.Floor(15 * (energy / 1.1f));
+		} else {
+			n = 15 + (int)Mathf.Floor(8 * (energy - 1.1f)/.9f);
+		}
+		string textureName = "ThrusterBottomHUD/thruster-bottom-hud-"+n;
+		Texture texture = (Texture) Resources.Load(textureName);
+		renderer.material.mainTexture = texture;
+	}
+}
