bc312997aaf7e30bd255a7e50131ead73fe67b69
Joel Lopez <jowie.jowie@gmail.com>
2018-01-28 01:49:59 +0100

    Add CRT post processing effect

---
+++ Assets/Scripts/CRT.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CRT : MonoBehaviour
+{
+    public Material material;
+    // Use this for initialization
+    void Start()
+    {
+        material = new Material(Shader.Find("Hidden/CRT"));
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetTexture("_MainTex", source);
+        Graphics.Blit(source, destination, material);
+    }
+}
